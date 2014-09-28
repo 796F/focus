6d77a524cc38ad548579250206f3333febf6d78f
Michael Xia <xia.umd@gmail.com>
2014-09-28 12:38:34 -0700

    Initial Commit

---
+++ Focus/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Focus
+//
+//  Created by Mike on 9/28/14.
+//  Copyright (c) 2014 Mike Xia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Focus/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Focus
+//
+//  Created by Mike on 9/28/14.
+//  Copyright (c) 2014 Mike Xia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
